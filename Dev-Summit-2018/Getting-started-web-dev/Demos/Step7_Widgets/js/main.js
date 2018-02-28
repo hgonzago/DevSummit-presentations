205ae8ccbe58e454d88df63fc0d6ead91f59eab5
Heather Gonzago <hgonzago@esri.com>
2018-02-27 22:10:02 -0500

    Updates made to getting started demos

--- Dev-Summit-2018/Getting-started-web-dev/Demos/Step7_Widgets/js/main.js
+++ Dev-Summit-2018/Getting-started-web-dev/Demos/Step7_Widgets/js/main.js
@@ -1,30 +1,26 @@
 require([
-    "esri/WebMap",
-    "esri/views/MapView",
-    "esri/widgets/Legend",
-    "esri/widgets/Search",
-    "dojo/domReady!"
+  "esri/WebMap",
+  "esri/views/MapView",
+  "esri/widgets/Legend",
+  "esri/widgets/Search",
+  "dojo/domReady!"
 ], function (WebMap, MapView, Legend, Search) {
-
   var map = new WebMap({
     portalItem: { // autocast
       id: "b45ec95aeeb743399dd4b557abd20ee2"
     }
   });
-
   var view = new MapView({
     container: "viewDiv",
     map: map,
     zoom: 3,
     center: [-99.14725260912257, 36.48617178360141]
   });
-
   /******************************************************************
    *
    * Widget example - Add legend widget
    *
    ******************************************************************/
-
   view.when(function () {
     var privateSchoolsPoly = map.layers.getItemAt(0);
     // Step 1: Create the widget
@@ -32,20 +28,30 @@ require([
       // Step 2: Specify any additional properties for the legend. In this case,
       // we are just setting the view to where the legend should apply
       view: view,
-      layerInfos: [
-        {
-          layer: privateSchoolsPoly,
-          title: "Private school enrollment"
-        }]
+      layerInfos: [{
+        layer: privateSchoolsPoly,
+        title: "Private school enrollment"
+      }]
     });
-
     var searchWidget = new Search({
-      view: view
+      view: view,
+      sources: [{
+        featureLayer: {
+          url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/OverlaySchools/FeatureServer/0",
+          outFields: ["*"],
+          popupTemplate: privateSchoolsPoly.popupTemplate
+        },
+        searchFields: ["state_abbr","state_name"],
+        displayField: "state_name",
+        exactMatch: false,
+        outFields: ["*"],
+        name: "State name",
+        placeholder: "example: Pennsylvania",
+        suggestionsEnabled: true
+      }]
     });
-
     // Step 3: Add the widget to the view's UI, specify the docking position as well
-
-      view.ui.add(legend, "bottom-left");
-      view.ui.add(searchWidget, "top-right");
+    view.ui.add(legend, "bottom-left");
+    view.ui.add(searchWidget, "top-right");
   });
 });
