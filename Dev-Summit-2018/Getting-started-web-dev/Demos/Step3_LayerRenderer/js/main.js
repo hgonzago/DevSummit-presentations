44579d7833c44bdc00447a5be6d19e457a3128b0
Heather Gonzago <hgonzago@esri.com>
2018-03-03 16:13:02 -0500

    Updates to getting started and integrate apps to portal

--- Dev-Summit-2018/Getting-started-web-dev/Demos/Step3_LayerRenderer/js/main.js
+++ Dev-Summit-2018/Getting-started-web-dev/Demos/Step3_LayerRenderer/js/main.js
@@ -1,14 +1,14 @@
 var view;
 require([
-    "esri/Map",
-    "esri/layers/FeatureLayer",
-    "esri/views/MapView",
-    "dojo/domReady!"
-], function (Map, FeatureLayer, MapView) {
-
+  "esri/Map",
+  "esri/layers/FeatureLayer",
+  "esri/views/MapView",
+  "dojo/domReady!"
+], function(Map, FeatureLayer, MapView) {
   var defaultSym = {
     type: "simple-fill", // autocasts as new SimpleFillSymbol()
-    outline: { // autocasts as new SimpleLineSymbol()
+    outline: {
+      // autocasts as new SimpleLineSymbol()
       color: "#a3acd1",
       width: 0.5
     }
@@ -26,36 +26,38 @@ require([
     type: "simple", // autocasts as new SimpleRenderer()
     symbol: defaultSym,
     label: "Private school enrollment ratio",
-    visualVariables: [{
-      type: "color",
-      field: "PrivateEnr",
-      stops: [
-        {
-          value: 0.044,
-          color: "#edf8fb",
-          label: "< 0.044"
+    visualVariables: [
+      {
+        type: "color",
+        field: "PrivateEnr",
+        stops: [
+          {
+            value: 0.044,
+            color: "#edf8fb",
+            label: "< 0.044"
           },
-        {
-          value: 0.059,
-          color: "#b3cde3"
+          {
+            value: 0.059,
+            color: "#b3cde3"
           },
-        {
-          value: 0.0748,
-          color: "#8c96c6",
-          label: "0.0748"
+          {
+            value: 0.0748,
+            color: "#8c96c6",
+            label: "0.0748"
           },
-        {
-          value: 0.0899,
-          color: "#8856a7"
+          {
+            value: 0.0899,
+            color: "#8856a7"
           },
-        {
-          value: 0.105,
-          color: "#994c99",
-          label: "> 0.105"
-          }]
-        }]
+          {
+            value: 0.105,
+            color: "#994c99",
+            label: "> 0.105"
+          }
+        ]
+      }
+    ]
   };
-
 
   /***********************************
    *  Create renderer for centroids
@@ -79,17 +81,18 @@ require([
 
   var privateSchoolsPoint = new FeatureLayer({
     // Private Schools centroids
-    url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Centroids/FeatureServer/0",
+    url:
+      "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Centroids/FeatureServer/0",
     renderer: centroidRenderer
   });
-
 
   var privateSchoolsPoly = new FeatureLayer({
     // Private schools per state
     // layer with rendering
     // url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/OverlaySchools/FeatureServer/0"
     // layer without rendering
-    url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/PrivateSchoolEnrollmentNoRendering/FeatureServer/0",
+    url:
+      "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/PrivateSchoolEnrollmentNoRendering/FeatureServer/0",
     outFields: ["*"],
     opacity: 0.8,
     renderer: renderer
@@ -107,10 +110,8 @@ require([
     center: [-99.14725260912257, 36.48617178360141]
   });
 
-  view.when(function () {
+  view.when(function() {
     map.addMany([privateSchoolsPoly, privateSchoolsPoint]);
     // map.add(privateSchoolsPoly);
   });
-
-
 });
--- Dev-Summit-2018/Getting-started-web-dev/Demos/Step7_Widgets/js/main.js
+++ Dev-Summit-2018/Getting-started-web-dev/Demos/Step7_Widgets/js/main.js
@@ -4,9 +4,10 @@ require([
   "esri/widgets/Legend",
   "esri/widgets/Search",
   "dojo/domReady!"
-], function (WebMap, MapView, Legend, Search) {
+], function(WebMap, MapView, Legend, Search) {
   var map = new WebMap({
-    portalItem: { // autocast
+    portalItem: {
+      // autocast
       id: "b45ec95aeeb743399dd4b557abd20ee2"
     }
   });
@@ -21,34 +22,38 @@ require([
    * Widget example - Add legend widget
    *
    ******************************************************************/
-  view.when(function () {
+  view.when(function() {
     var privateSchoolsPoly = map.layers.getItemAt(0);
     // Step 1: Create the widget
     var legend = new Legend({
       // Step 2: Specify any additional properties for the legend. In this case,
       // we are just setting the view to where the legend should apply
       view: view,
-      layerInfos: [{
-        layer: privateSchoolsPoly,
-        title: "Private school enrollment"
-      }]
+      layerInfos: [
+        {
+          layer: privateSchoolsPoly,
+          title: "Private school enrollment"
+        }
+      ]
     });
     var searchWidget = new Search({
       view: view,
-      sources: [{
-        featureLayer: {
-          url: privateSchoolsPoly.url,
+      sources: [
+        {
+          featureLayer: {
+            url: privateSchoolsPoly.url,
+            outFields: ["*"],
+            popupTemplate: privateSchoolsPoly.popupTemplate
+          },
+          searchFields: ["state_abbr", "state_name"],
+          displayField: "state_name",
+          exactMatch: false,
           outFields: ["*"],
-          popupTemplate: privateSchoolsPoly.popupTemplate
-        },
-        searchFields: ["state_abbr","state_name"],
-        displayField: "state_name",
-        exactMatch: false,
-        outFields: ["*"],
-        name: "State name",
-        placeholder: "example: Pennsylvania",
-        suggestionsEnabled: true
-      }]
+          name: "State name",
+          placeholder: "Search by state name",
+          suggestionsEnabled: true
+        }
+      ]
     });
     // Step 3: Add the widget to the view's UI, specify the docking position as well
     view.ui.add(legend, "bottom-left");
