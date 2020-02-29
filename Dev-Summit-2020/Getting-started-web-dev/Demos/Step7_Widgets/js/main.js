91e02ea79e921884753f945442ae3e3293f586b0
Heather Gonzago <hgonzago@esri.com>
2020-02-28 23:00:38 -0500

    intro session - renamed demo 6 to 7 (widgets)

--- Dev-Summit-2020/Getting-started-web-dev/Demos/Step7_Widgets/js/main.js
+++ Dev-Summit-2020/Getting-started-web-dev/Demos/Step7_Widgets/js/main.js
@@ -3,15 +3,15 @@ require([
   "esri/views/MapView",
   "esri/widgets/Legend",
   "esri/widgets/Swipe",
-], function(WebMap, MapView, Legend, Swipe) {
+], function (WebMap, MapView, Legend, Swipe) {
 
-  var map = new WebMap({
+  const map = new WebMap({
     portalItem: {
       // autocast
       id: "f9a9a7e3857d4d51b2c801cf8c399add"
     }
   });
-  var view = new MapView({
+  const view = new MapView({
     container: "viewDiv",
     map: map
   });
@@ -20,17 +20,16 @@ require([
    * Widget example - Add legend widget
    *
    ******************************************************************/
-  view.when(function() {
-    var chicagoCrime = map.layers.getItemAt(0);
-    var vehicles = map.layers.getItemAt(1);
+  view.when(function () {
+    const chicagoCrime = map.layers.getItemAt(0); // bottom
+    const vehicles = map.layers.getItemAt(1); // top of TOC
     vehicles.visible = true;
     // Step 1: Create the widget
-    var legend = new Legend({
+    const legend = new Legend({
       // Step 2: Specify any additional properties for the legend. In this case,
       // we are just setting the view to where the legend should apply
       view: view,
-      layerInfos: [
-        {
+      layerInfos: [{
           layer: chicagoCrime,
           title: "Chicago Crime Tracts"
         },
@@ -41,13 +40,13 @@ require([
       ]
     });
 
-    var swipe = new Swipe({
+    const swipe = new Swipe({
       view: view,
       leadingLayers: [chicagoCrime],
       trailingLayers: [vehicles],
       position: 45
     });
-    
+
     // Step 3: Add the widget to the view's UI, specify the docking position as well
     view.ui.add(legend, "top-right");
     view.ui.add(swipe);
